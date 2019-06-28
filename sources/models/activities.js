fa7e631288425ca9434ad7252d1ecfec96e062f8
Dzmitry Litsvinau <dimalitx@gmail.com>
2019-06-28 11:23:12 +0300

    fix: fix dates, remove deletetion of a property

--- sources/models/activities.js
+++ sources/models/activities.js
@@ -1,23 +1,24 @@
+const formatToDate = webix.Date.strToDate("%d-%m-%Y %H:%i");
+const formatToStr = webix.Date.dateToStr("%Y-%m-%d %H:%i");
+
 export const activities = new webix.DataCollection({
 	url: "http://localhost:8096/api/v1/activities/ ",
 	save: "rest->http://localhost:8096/api/v1/activities/",
 	scheme: {
 		$init: (obj) => {
-			let formatToDate = webix.Date.strToDate("%d-%m-%Y %H:%i");
 			if (!obj.convertedDate) {
-				obj.convertedDate = new Date(formatToDate(obj.DueDate));
+				obj.convertedDate = formatToDate(obj.DueDate);
 			}
 			if (!obj.convertedTime) {
-				obj.convertedTime = new Date(obj.convertedDate);
+				obj.convertedTime = obj.convertedDate;
 			}
 		},
 		$save: (obj) => {
-			let formatToStr = webix.Date.dateToStr("%Y-%m-%d %H:%i");
-			obj.DueDate = new Date(obj.convertedDate);
-			obj.DueTime = new Date(obj.convertedTime);
-			obj.DueDate.setHours(obj.DueTime.getHours());
-			obj.DueDate.setMinutes(obj.DueTime.getMinutes());
-			obj.DueDate = formatToStr(obj.DueDate);
+			let time = obj.convertedTime;
+			let date = obj.convertedDate;
+			date.setHours(time.getHours());
+			date.setMinutes(time.getMinutes());
+			obj.DueDate = formatToStr(obj.convertedDate);
 		}
 	}
 });
